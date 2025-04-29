ed6af9c9031694b04f3802f42d497e91bd331395
belli <bellinna.uong@outlook.com>
2025-04-29 12:14:49 +0200

    clean

--- detect_politesse.py
+++ detect_politesse.py
@@ -3,17 +3,14 @@ def detecter_politesse(texte):
     with open('liste/politesse.txt', 'r', encoding='utf-8') as f:
         mots_politesse = [ligne.strip() for ligne in f.readlines()]
     
-    # Vérifier si le texte contient un mot de politesse
     for mot in mots_politesse:
         if mot.lower() in texte.lower():
             return True
     return False
 
-# Demander l'input à l'utilisateur
 texte = input("Entrez votre texte : ")
 
-# Vérifier et afficher le résultat
 if detecter_politesse(texte):
-    print("Il ne faut pas être poli !")
+    print("Je suis une IA pas ton père, pas besoin d'être poli...")
 else:
     print("Le texte ne contient pas de mots de politesse.") 
